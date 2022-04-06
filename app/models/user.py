07856478a6ca720eb2cfe3a2e773f00a46550376
crpier <crpier42@gmail.com>
2022-04-06 23:46:58 +0300

    get rid of item models

--- app/models/user.py
+++ app/models/user.py
@@ -5,9 +5,6 @@ from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
 
-if TYPE_CHECKING:
-    from .item import Item  # noqa: F401
-
 
 class User(Base):
     __tablename__ = "users"  # type: ignore # noqa
@@ -18,4 +15,3 @@ class User(Base):
     hashed_password = Column(String(255), nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
-    items = relationship("Item", back_populates="owner")
